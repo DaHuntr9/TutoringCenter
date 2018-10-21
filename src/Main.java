d1b30904c0d7f34c99b96cba888ec0ecee46a6ae
Hunter <hunterdstudent@gmail.com>
2018-10-21 16:47:52 -0400

    Removed files that are not relevent to the fxml and controller files

--- src/Main.java
+++ src/Main.java
@@ -15,7 +15,7 @@ public class Main extends Application {
 
   @Override
   public void start(Stage primaryStage) throws Exception {
-    Parent root = FXMLLoader.load(getClass().getResource("signin.fxml"));
+    Parent root = FXMLLoader.load(getClass().getResource("signup.fxml"));
     primaryStage.setTitle("Tutoring App");
     primaryStage.setScene(new Scene(root, 600, 400));
     primaryStage.show();
